0465dbe58bf271b87a7f05d91b0038264ef05dda
SokolovNikita2530 <iiiiiiii2281337@gmail.com>
2024-11-24 22:10:57 +0300

    completed the second laboratory work on the C language

--- C/LAB_2/index.c
+++ C/LAB_2/index.c
@@ -2,141 +2,112 @@
 #include <stdlib.h>
 #include <time.h>
 #include <float.h>
-#include <string.h>
 
-#define N 20
-
-void generate_matrix(float matrix[N][N], int n)
+void generate_matrix(float **matrix, int n)
 {
 	srand(time(NULL));
 
 	for (int i = 0; i < n; i++)
-        {
-                for (int j = 0; j < n; j++)
-                {
-                        matrix[i][j] = 0.0f + (rand() / (float)RAND_MAX) * (10.0f - 0.0f);
-                }
-        }
+        for (int j = 0; j < n; j++)
+            matrix[i][j] = 0.0f + (rand() / (float)RAND_MAX) * (10.0f - 0.0f);
 }
 
 
-void print_matrix(float matrix[N][N], int n)
+void print_matrix(float **matrix, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
-		{
-			printf("%f ", matrix[i][j]);
-		}
+			printf("%.2f ", matrix[i][j]);
+
 		printf("\n");
 	}
 }
 
-void enter_matrix(float matrix[N][N], int n)
+void enter_matrix(float **matrix, int n)
 {
 	for (int i = 0; i < n; i++)
-        {
-                for (int j = 0; j < n; j++)
-                {
-                        scanf("%f", &matrix[i][j]);
-                }
-        }
+        for (int j = 0; j < n; j++)
+            if(scanf("%f", &matrix[i][j]) != 1)
+			{
+				printf("Invalid input");
+				exit(1);
+			}
 }
 
-void find_area_min_and_generate_matrix(float matrix[N][N], int n, int pos_i, int pos_j)
+void find_area_min_and_generate_matrix(float **matrix, float **transformed_matrix, int n)
 {
-	if (pos_i > n && pos_j > n)
-	{
-		print_matrix(matrix, n);
-	}
-	else 
-	{
-		float area_min = FLT_MAX;
+	for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
+            float area_min = FLT_MAX;
 
-		for (int i = pos_i; i < n; i++) 
-		{	
-			int start_pos, end_pos;
-			
-			if (pos_j - (i - pos_i) < 0)
-			{
-				start_pos = 0;
-			}
-			else
-			{
-				start_pos = pos_j - (i - pos_i);
-			}
+            for (int k = i; k < n; k++) {
+                int offset = k - i;
 
-			if (pos_j + (i - pos_i) >= n)
-			{
-				end_pos = n - 1;	
-			}
-			else
-			{
-				end_pos = pos_j + (i - pos_i);
-			}
+                int start_pos = (j - offset < 0) ? 0 : j - offset;
+                int end_pos = (j + offset >= n) ? n - 1 : j + offset;
 
-			for (int j = start_pos; j <= end_pos; j++)
-			{
-				if (matrix[i][j] < area_min)
-				{
-					area_min = matrix[i][j];
-				}
-			}
-		}
-		
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				matrix[i][j] = area_min;
-			}
-		}
-	}	
+                for (int l = start_pos; l <= end_pos; l++) 
+					if (area_min > matrix[k][l]) area_min = matrix[k][l];
+            }
+
+            transformed_matrix[i][j] = area_min;
+        }
 }	
 
 int main(void)
 {
 	int n;
-	float matrix[N][N];
-	char ans[10];
+	float **matrix, **transformed_matrix;
 
 	printf("Enter matrix size from 1 to 20 inclusive (N x N): ");
-	scanf("%d", &n);
-
+	if (scanf("%d", &n) != 1) 
+    {
+        printf("Invalid input.\n");
+        return 1;
+    }
+	
 	if (n > 20 || n < 1)
 	{
 		printf("Wrong matrix size!\n");
 		return 1;
 	}
 	
-	if (n > 5)
-	{	
-		printf("Generate a matrix? (Y/N): ");
-		scanf("%9s", ans);
-		if (strcmp(ans, "Y") == 0)
-		{	
-			generate_matrix(matrix, n);	
-		}
-		else if (strcmp(ans, "N") == 0){
-			printf("Enter matrix elements: \n");
-                        enter_matrix(matrix, n);
-		}
-		else {
-			printf("Ok, bro, no, it's not.");
-                        printf("\n");
-                        printf("Enter matrix elements: \n");
-                        enter_matrix(matrix, n);
-		}
-	}
-	
+	matrix = (float**)malloc(n * sizeof(float*));
+	if (matrix == NULL) return -1;
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i] = (float*)malloc(n * sizeof(float));
+		if (matrix[i] == NULL) return -1;
+       	} 
+
+	transformed_matrix = (float**)malloc(n * sizeof(float*));
+	if (transformed_matrix == NULL) return -1;
+	for (int i = 0; i < n; i++)
+	{
+		transformed_matrix[i] = (float*)malloc(n * sizeof(float));
+		if (transformed_matrix[i] == NULL) return -1;
+       	} 
+
+	//enter_matrix(matrix, n);
+	generate_matrix(matrix, n);
+	find_area_min_and_generate_matrix(matrix, transformed_matrix, n);
+
 	printf("Enered matrix: \n");
 	print_matrix(matrix, n);
 	printf("\n");
 
 	printf("Transformed matrix: \n");
-	find_area_min_and_generate_matrix(matrix, n, 4, 3);
-	print_matrix(matrix, n);
+	print_matrix(transformed_matrix, n);
 	printf("\n");
+
+	for (int i = 0; i < n; i++)
+    {
+        free(matrix[i]);
+        free(transformed_matrix[i]);
+    }
+    free(matrix);
+    free(transformed_matrix);
 	
 	return 0;
 }
